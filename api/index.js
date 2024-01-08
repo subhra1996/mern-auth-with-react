ae17bcc611758893989be3cad054f5ae4353a5b8
subhra1996 <subhrajitdebroy2017@gmail.com>
2024-01-09 01:58:42 +0530

    created middleware to handle error

--- api/index.js
+++ api/index.js
@@ -24,3 +24,15 @@ app.listen(3000, ( )=>{
 app.use('/api/user', userRoutes);
 
 app.use('/api/auth', authRoutes);
+
+
+app.use((error, req, res, next)=>{
+    const statusCode = error.statusCode || 500;
+    const message = error.message || 'something went wrong';
+
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    });
+});
