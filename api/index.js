64dc850b8b8446f3a6af56e44584cc3b63f991d3
subhra1996 <subhrajitdebroy2017@gmail.com>
2024-01-08 19:43:36 +0530

    creater signup functionality

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import Userroutes from './routes/user.route.js';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -14,8 +15,12 @@ mongoose.connect(process.env.MONGO)
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, ( )=>{
     console.log('Linstening on port 3000');
 });
 
-app.use('/api/user', Userroutes);
+app.use('/api/user', userRoutes);
+
+app.use('/api/auth', authRoutes);
