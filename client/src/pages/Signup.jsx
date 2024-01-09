05fedff21f61d61b92de06823b8c54cf2c9c9956
subhra1996 <subhrajitdebroy2017@gmail.com>
2024-01-09 17:07:16 +0530

    complete sign up page functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -3,15 +3,45 @@ import { Link } from "react-router-dom";
 
 const Signup = () => {
   const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try{
+      setLoading(true);
+      setError(false);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(formData)
+      });
+
+      const data = await res.json();
+
+      setLoading(false);
+      if(data.success===false){
+        setError(true);
+      }
+
+    }
+    catch(error){
+        setLoading(false);
+        setError(true);
+    }
+  };
+
   return (
     <div className="max-w-lg mx-auto p-3">
       <h1 className="text-3xl font-semibold text-center my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Username"
@@ -36,8 +66,8 @@ const Signup = () => {
           onChange={handleChange}
         />
 
-        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+          {loading ? 'Loading...' : 'Sign Up' }
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -46,6 +76,8 @@ const Signup = () => {
           <span className="text-blue-500">Sign in</span>
         </Link>
       </div>
+
+      <p className="text-red-700 mt-5">{error && 'Something went wrong!'}</p>
     </div>
   );
 };
